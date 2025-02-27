813d49abef09f35c8e5cab776f4ee2035b206687
VinayChaurasia01 <vinaychaurasia097@gmail.com>
2025-02-27 15:50:08 +0530

    [Vinay Chaurasia] Add . Employee Controller for CURD operation by springBoot

--- Employee-Payroll-App/src/main/java/com/employeepayroll/controller/EmployeeController.java
+++ Employee-Payroll-App/src/main/java/com/employeepayroll/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.employeepayroll.controller;
 import com.employeepayroll.model.Employee;
 import com.employeepayroll.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,10 +23,10 @@ class EmployeeController {
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) { return service.saveEmployee(employee); }
 
-    @PutMapping("/{id}")
-    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
-        employee.setId(id);
-        return service.saveEmployee(employee);
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        Employee updatedEmployee = service.updateEmployee(id, employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 
     @DeleteMapping("/{id}")
